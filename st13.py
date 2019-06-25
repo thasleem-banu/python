0567849b786ff25318978ca8f65d912cf7783a1a
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-06-25 11:33:03 +0530

    Update st13.py

--- st13.py
+++ st13.py
@@ -1,4 +1,4 @@
-asd=raw_input("Enter variable")
+asd=raw_input()
 if(asd=='A' or asd=='a' or asd=='E' or asd=='e' or asd=='I' or asd=='i' or asd=='O' or asd=='o' or asd=='U' or asd=='u'):
   print("Vowel")
 elif(asd!='A' or asd!='a' or asd!='E' or asd!='e' or asd!='I' or asd!='i' or asd!='O' asd!='o' or asd!='U' or asd!='u'):
