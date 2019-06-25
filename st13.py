eb5567ab82f8e41cee487c33b63dd77869ab2ebc
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-06-25 11:10:39 +0530

    Create st13.py

---
+++ st13.py
@@ -0,0 +1,6 @@
+as=input()
+if(as=='A' or as=='a' or as=='E' or as=='e' or as=='I' or as=='i' or as=='O' or as=='o' or as=='U' or as=='u'):
+  print("Vowel")
+elif(as!='A' or as!='a' or as!='E' or as!='e' or as!='I' or as!='i' or as!='O' as!='o' or as!='U' or as!='u'):
+  print("Consonant")
+else  print("invalid")
