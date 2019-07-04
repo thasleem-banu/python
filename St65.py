f0ff50c1a3569feb5f47c1d6fa7cbab1c0762cc1
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-04 23:05:49 +0530

    Update and rename St65.py to St65thas.py

--- St65.py
+++
@@ -1,6 +0,0 @@
-k11,k21=map(int,input().split())
-k31=k11*k21
-if(k31%2==0):
- print("even")
-else:
- print("odd")
---
+++ St65thas.py
@@ -0,0 +1,6 @@
+t11,t21=map(int,input().split())
+t31=t11*t21
+if(t31%2==0):
+ print("even")
+else:
+ print("odd")
