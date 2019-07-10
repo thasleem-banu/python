6dbfe4ab64b5aaa6ed1078d565ec0bf83e6313a0
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-10 22:40:52 +0530

    Update Aaapro27.py

--- Aaapro27.py
+++ Aaapro27.py
@@ -6,4 +6,4 @@ for i in range(len(bent1)):
     for j in range(len(bent1)):
         if bent1[i]+bent1[j] == sent1:
             fake1 = True
-print("yes" if fake else "no")
+print("yes" if fake1 else "no")
