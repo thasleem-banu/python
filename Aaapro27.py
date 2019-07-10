3ba3a80cc732f9332430968d13e7524f1fb0acdd
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-10 22:38:13 +0530

    Create Aaapro27.py

---
+++ Aaapro27.py
@@ -0,0 +1,9 @@
+bhavt1,sent1 = input().split()
+sent1 = int(sent1)
+fake1 = False
+bent1 = list(map(int,input().split()))
+for i in range(len(bent1)):
+    for j in range(len(bent1)):
+        if bent1[i]+bent1[j] == sent1:
+            fake1 = True
+print("yes" if fake else "no")
