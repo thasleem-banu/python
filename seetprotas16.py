90cca88152d5dfb9f155a3335a69a8983088bbc9
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-10 16:43:59 +0530

    Update and rename seetprothas16.py to seetprotas16.py

--- seetprotas16.py
+++ seetprotas16.py
@@ -3,7 +3,7 @@ sha1=list(map(int,input().split()))
 h1=0
 for g in range(len(sha1)-2):
     for p in range(g+1,len(sha1)-1):
-        for love in range(p+1,len(sha1)):
+        for love1 in range(p+1,len(sha1)):
             if sha1[g]<sha1[p]<sha1[love1] and g<p<love1:
                 h1=h1+1
 print(h1)
