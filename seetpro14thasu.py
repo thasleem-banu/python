fb313331f5a2dedce4c91dcd9b3b0794457058a9
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-10 16:18:50 +0530

    Update and rename seetpro14thas.py to seetpro14thasu.py

--- seetpro14thasu.py
+++ seetpro14thasu.py
@@ -4,7 +4,7 @@ if len(vs12)>len(vit112):
   vs12,vit112=vit112,dbj12
 i=0
 while i<len(vs12):
-  was12+=(ord(vit112[i])-ord(vs1[i]))
+  was12+=(ord(vit112[i])-ord(vs12[i]))
   i+=1
 for i in range(i,len(vit12)):
   was12+=ord(vit112[i])-ord('a')+1
