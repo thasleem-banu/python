f6605205df554b8edda55d316adc128112c10bfb
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-16 16:42:40 +0530

    Update Aaasetpro31.py

--- Aaasetpro31.py
+++ Aaasetpro31.py
@@ -1,7 +1,7 @@
 pp1 = int(input())
 qq1 = list(map(int, input().split()))
 r1 = int(len(qq1)/2)
-if sum(qq1[:r])//len(qq1[:r]) == sum(qq1[r:])//len(qq1[r:]):
+if sum(qq1[:r])//len(qq1[:r1]) == sum(qq1[r1:])//len(qq1[r1:]):
     print('yes')
 else:
     print('no')
