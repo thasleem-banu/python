7b02748823c8c314fc77be111df21c86d3a1fe8f
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-16 16:58:06 +0530

    Create Aaasetpro35.py

---
+++ Aaasetpro35.py
@@ -0,0 +1,18 @@
+n1=int(input())
+m=input()
+o=m.split(" ")
+p=[]
+for i in o:
+    p.append(int(i))
+q=[]
+count=1
+for j in range(0,n-1):
+    if p[j]<p[j+1]:
+        count+=1
+    else:
+        q.append(count)
+        
+        count=1
+    
+q.append(count)
+print(max(q))
