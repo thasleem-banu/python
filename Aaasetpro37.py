22c172f5c36baebe9b72d10acd604b086d818c5f
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-19 20:35:47 +0530

    Create Aaasetpro37.py

---
+++ Aaasetpro37.py
@@ -0,0 +1,17 @@
+Aa1,Bb=map(int,input().split())
+Cc=list(map(int,input().split()))
+pr=list(map(int,input().split()))
+qr=[]
+ar=0
+for i in range(Aa1):
+    x=pr[i]/Cc[i]
+    qr.append(x)
+while Bb>=0 and len(qr)>0:
+    mindex=qr.index(max(qr))
+    if Bb>=Cc[mindex]:
+        ar=ar+pr[mindex]
+        Bb=Bb-Cc[mindex]
+    Cc.pop(mindex)
+    pr.pop(mindex)
+    qr.pop(mindex)
+print(ar)
