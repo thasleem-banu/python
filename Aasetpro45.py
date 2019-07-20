9f34273633feb0f67fe57a015206cdcf3fb39562
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-20 18:35:44 +0530

    Update and rename Aaasetpro45.py to Aasetpro45.py

--- Aasetpro45.py
+++ Aasetpro45.py
@@ -1,12 +1,12 @@
 x1,y=map(int,input().split())
 mn1=0
-L1=[]
+L11=[]
 for i in range(x1):
-      L1.append(input())
+      L11.append(input())
 for i in range(x1):
       for j in range(y-1):
-            if(L1[i][j]!='R' and L1[i][j+1]!='R'):
+            if(L11[i][j]!='R' and L11[i][j+1]!='R'):
                   mn1+=3
-            elif(L1[i][j]!='G' and L1[i][j+1]!='G'):
+            elif(L11[i][j]!='G' and L11[i][j+1]!='G'):
                   mn1+=5
 print(mn1)
