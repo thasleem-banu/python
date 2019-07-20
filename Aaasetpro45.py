119b0bfda7023c619217b77b8de177674b964876
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-20 18:34:24 +0530

    Update Aaasetpro45.py

--- Aaasetpro45.py
+++ Aaasetpro45.py
@@ -1,9 +1,9 @@
-x,y=map(int,input().split())
+x1,y=map(int,input().split())
 mn1=0
 L1=[]
-for i in range(x):
+for i in range(x1):
       L1.append(input())
-for i in range(x):
+for i in range(x1):
       for j in range(y-1):
             if(L1[i][j]!='R' and L1[i][j+1]!='R'):
                   mn1+=3
