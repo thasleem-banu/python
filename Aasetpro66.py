104ecbb29dda055ac51e316f71e204a0c81fcf8f
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-28 19:59:11 +0530

    Create Aasetpro66.py

---
+++ Aasetpro66.py
@@ -0,0 +1,12 @@
+  
+import sys,string, math, itertools
+
+nt1,kt = input().split()
+nt1,kt = int(nt1),int(kt)
+Leet = [ int(x) for x in input().split()]
+#print(nt1,kt, Leet)
+for i in range(0,nt) :
+    if (86400-Leet[i]) >= kt :
+        print(i+1)
+        sys.exit()
+    kt -= (86400-Leet[i])
