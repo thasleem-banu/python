1b92443e3ca426449813eb1b521a64b8ca525efe
thasleem-banu <51981012+thasleem-banu@users.noreply.github.com>
2019-07-28 20:16:22 +0530

    Update and rename Aasetpro66.py to Aaasetpro66.py

--- Aaasetpro66.py
+++ Aaasetpro66.py
@@ -5,7 +5,7 @@ nt1,kt = input().split()
 nt1,kt = int(nt1),int(kt)
 Leet = [ int(x) for x in input().split()]
 #print(nt1,kt, Leet)
-for i in range(0,nt) :
+for i in range(0,nt1) :
     if (86400-Leet[i]) >= kt :
         print(i+1)
         sys.exit()
